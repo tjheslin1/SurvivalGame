5fe6a4a32bfe440e2f7a073adfe948d9fd19d5fd
Thomas Heslin <tjheslin1@gmail.com>
2016-12-17 13:56:03 +0000

    MainWindow produced

--- main.go
+++ main.go
@@ -1,7 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"engo.io/engo"
+	"engo.io/ecs"
+)
+
+type myScene struct{}
+
+func (*myScene) Type() string {
+	return "myGame"
+}
+
+// Preload is called before loading any assets from the disk,
+// to allow you to register / queue them
+func (*myScene) Preload() {
+
+}
+
+// Setup is called before the main loop starts. It allows you
+// to add entities and systems to your Scene.
+func (*myScene) Setup(*ecs.World) {
+
+}
 
 func main() {
-	fmt.Println("Survival Game!")
+	opts := engo.RunOptions{
+		Title: "Hello World",
+		Width:  400,
+		Height: 400,
+	}
+	engo.Run(opts, &myScene{})
 }
